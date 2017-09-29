62c2c9d50a42b8a57800793228f252bc2547f16c
Muhammad Hassaan Bashir <eeng.hassaan@gmail.com>
2017-09-29 22:34:28 +0500

    Committing Attempt

---
+++ app/src/main/java/com/example/lenovo/jnoor/VersionModel.java
@@ -0,0 +1,13 @@
+package com.example.lenovo.jnoor;
+
+/**
+ * Created by LENOVO on 9/28/2017.
+ */
+public class VersionModel {
+    public static final String[] data = {
+            "cupcake", "donut", "eclair", "froyo", "gingerbread",
+            "honeycomb", "ice cream sandwich", "jelly bean", "kit kat",
+            "Lollipop", "marshmallow"
+    };
+
+}
